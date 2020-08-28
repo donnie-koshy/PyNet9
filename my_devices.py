a134c75892efe94e1d4d0ea07efc5a5575d5e032
Donnie Koshy <donnie.koshy@gmail.com>
2020-08-28 14:27:52 -0700

    Exercise 1a

---
+++ my_devices.py
@@ -0,0 +1,17 @@
+#!usr/bin/env python
+from getpass import getpass
+
+cisco3={
+"hostname":"cisco3.lasthop.io",
+"username":"pyclass",
+"password":getpass(),
+"optional_args":{}
+}
+
+arista1=dict(
+ "hostname"="arista1.lasthop.io",
+"username"="pyclass",
+"password"=getpass().
+"optional_args"={}
+}
+
